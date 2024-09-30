8274d52d54ad8a20441d6673630cd64a07efd3a9
Satya Naraharisetty <venkatnaraharisetty1234@gmail.com>
2024-09-30 13:55:36 +0530

    .

--- src/main/java/com/project/hospital_management_system/main.java
+++ src/main/java/com/project/hospital_management_system/main.java
@@ -5,8 +5,8 @@ import java.io.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
-public class HelloServlet extends HttpServlet {
+@WebServlet("/hello-servlet")
+public class main extends HttpServlet {
     private String message;
 
     public void init() {
